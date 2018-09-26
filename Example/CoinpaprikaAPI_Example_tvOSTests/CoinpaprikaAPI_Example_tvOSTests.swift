22369b5ba9a2b478cf818dbf8b338de98ca0c300
Dominique Stranz <dstranz@greywizard.com>
2018-09-26 12:43:42 +0200

    add macOS & tvOS tests

---
+++ Example/CoinpaprikaAPI_Example_tvOSTests/CoinpaprikaAPI_Example_tvOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  CoinpaprikaAPI_Example_tvOSTests.swift
+//  CoinpaprikaAPI_Example_tvOSTests
+//
+//  Created by Dominique Stranz on 26/09/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import XCTest
+@testable import CoinpaprikaAPI_Example_tvOS
+
+class CoinpaprikaAPI_Example_tvOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Example/Pods/CoinpaprikaAPI tvOS/CoinpaprikaAPI_tvOS.h
@@ -0,0 +1,18 @@
+//
+//  CoinpaprikaAPI_tvOS.h
+//  CoinpaprikaAPI tvOS
+//
+//  Created by Dominique Stranz on 26/09/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoinpaprikaAPI_tvOS.
+FOUNDATION_EXPORT double CoinpaprikaAPI_tvOSVersionNumber;
+
+//! Project version string for CoinpaprikaAPI_tvOS.
+FOUNDATION_EXPORT const unsigned char CoinpaprikaAPI_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoinpaprikaAPI_tvOS/PublicHeader.h>
+
+

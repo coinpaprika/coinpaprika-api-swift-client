82cfe5b72f1c89e4b6a6227740944cf42d73dfbd
Dominique Stranz <dstranz@greywizard.com>
2019-06-27 15:33:47 +0200

    Add exclude to SPM

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 // swift-tools-version:5.1
 import PackageDescription
 
+let excludeList = ["Example", "docs", "fastlane"]
+
 let package = Package(
     name: "Coinpaprika",
     platforms: [.iOS(.v10), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v3)],
@@ -12,16 +14,19 @@ let package = Package(
         .target(
             name: "Coinpaprika",
             dependencies: [.target(name: "Networking")],
-            path: "CoinpaprikaAPI/Classes/Client"
+            path: "CoinpaprikaAPI/Classes/Client",
+            exclude: excludeList
             ),
         .target(
             name: "Networking",
-            path: "CoinpaprikaAPI/Classes/Networking"
+            path: "CoinpaprikaAPI/Classes/Networking",
+            exclude: excludeList
         ),
         .testTarget(
             name: "CoinpaprikaTests",
             dependencies: ["Coinpaprika"],
-            path: "Example/Tests"
+            path: "Example/Tests",
+            exclude: excludeList
         )
     ]
 )
